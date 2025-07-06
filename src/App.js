7da55d4d8e238fa46164679a3ae20269213b3d27
BrahmamStudent <sakamuriveera@gmail.com>
2025-07-06 23:03:33 +0530

    Initial React setup with routing and Redux provider

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './Components/LoginComponent/Login';
+import Home from './Components/HomeComponent/Home';
+import Layout from './Components/LayoutComponent/Layout';
 
 const App = () => {
+  const isUserLogin = JSON.parse(localStorage.getItem("isLoggedIn"));
+
   return (
-    <div>
-      App data
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        {isUserLogin && (
+          <Route path="/layouthome" element={<Layout />}>
+            <Route index element={<Home />} />
+          </Route>
+        )}
+      </Routes>
+    </BrowserRouter>
   );
 };
 
